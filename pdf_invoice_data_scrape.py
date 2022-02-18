7f6b35ee328abc5e2b069e8ed29a4059a14f667a
munasef95 <53122404+munasef95@users.noreply.github.com>
2022-02-18 11:57:11 -0500

    Add files via upload

--- pdf_invoice_data_scrape.py
+++ pdf_invoice_data_scrape.py
@@ -6,31 +6,30 @@ import cv2
 
 
 class ocrByImageRegion:
-    key_words_data_lists = []
 
-    def __init__(self, file_path, folder_path = ''):
+    def __init__(self, file_path = ''):
         self.file_path = file_path
-        self.folderpath = folder_path
-        ocrByImageRegion.get_getword_coordinates(self)
+        self.key_words_data_lists = []
+        self.get_getword_coordinates()
+        
 
     #compiles
-    def get_getword_coordinates(self):
+    def get_getword_coordinates(self): 
         img_lines = tess.image_to_data(self.file_path)
         keyWords = ['CUSTOMER', 'INFORMATION:', 'JOB', 'DISCIPLINE:', 'PHONE:', 'EMAIL:']
-        ocrByImageRegion.key_words_data_lists.clear()
         for line in img_lines.splitlines():
             line_data = line.split()
             if len(line_data) == 12:
                 if line_data[11].upper() in keyWords:
                     del line_data[0:6]
-                    ocrByImageRegion.key_words_data_lists.append(line_data)
-        return ocrByImageRegion.key_words_data_lists
+                    self.key_words_data_lists.append(line_data)
+        return self.key_words_data_lists
     
     
     #compiles
-    def create_roi(self, keyword, image, index, xoffset, yoffset, xroi, yroi):
-        x = int(ocrByImageRegion.key_words_data_lists[index][0]) - xoffset
-        y = int(ocrByImageRegion.key_words_data_lists[index][1]) + yoffset
+    def create_roi(self, image, index, xoffset, yoffset, xroi, yroi):
+        x = int(self.key_words_data_lists[index][0]) - xoffset
+        y = int(self.key_words_data_lists[index][1]) + yoffset
         name_roi = image[y: y + yroi, x: x + xroi]
         #cv2.imshow(keyword + ' WINDOW', name_roi)
         #cv2.waitKey(0)
@@ -51,49 +50,48 @@ class ocrByImageRegion:
     def get_customer_name (self):
         img = cv2.imread(self.file_path, 0)
         x, y = 0, 0
-        for i, list in enumerate(ocrByImageRegion.key_words_data_lists):
-            keyword_from_array = ocrByImageRegion.key_words_data_lists[i][5].upper()
+        for i, list in enumerate(self.key_words_data_lists):
+            keyword_from_array = self.key_words_data_lists[i][5].upper()
             if keyword_from_array == 'CUSTOMER':
-                return ocrByImageRegion.create_roi(self, keyword_from_array, img, i, 10, 15, 200, 20)
+                return self.create_roi(img, i, 10, 15, 200, 20)
 
     #compiles with create_roi()
     def get_customer_email (self):
         img = cv2.imread(self.file_path, 0)
-        for i, list in enumerate(ocrByImageRegion.key_words_data_lists):
-            keyword_from_array = ocrByImageRegion.key_words_data_lists[i][5].upper()
+        for i, list in enumerate(self.key_words_data_lists):
+            keyword_from_array = self.key_words_data_lists[i][5].upper()
             if keyword_from_array == 'EMAIL:' :
-                return ocrByImageRegion.create_roi(self, keyword_from_array, img, i, -30, -5, 150, 20)
+                return self.create_roi(img, i, -30, -5, 150, 20)
         
 
     #compiles with create_roi()        
     def get_customer_address(self):
         img = cv2.imread(self.file_path, 0)
-        for i, list in enumerate(ocrByImageRegion.key_words_data_lists):
-            keyword_from_array = ocrByImageRegion.key_words_data_lists[i][5].upper()
+        for i, list in enumerate(self.key_words_data_lists):
+            keyword_from_array = self.key_words_data_lists[i][5].upper()
             if keyword_from_array == 'CUSTOMER' :
-                return ocrByImageRegion.create_roi(self, keyword_from_array, img, i, 10, 30, 150, 40)
+                return self.create_roi(img, i, 10, 30, 150, 40)
             
 
     #compiles with create_roi()        
     def get_job_type(self):
         img = cv2.imread(self.file_path, 0)
         x, y = 0, 0
-        for i, list in enumerate(ocrByImageRegion.key_words_data_lists):
-            keyword_from_array = ocrByImageRegion.key_words_data_lists[i][5].upper()
+        for i, list in enumerate(self.key_words_data_lists):
+            keyword_from_array = self.key_words_data_lists[i][5].upper()
             if keyword_from_array == 'DISCIPLINE:':
-                return ocrByImageRegion.create_roi(self, keyword_from_array, img, i, -52, -5, 165, 18)
+                return self.create_roi(img, i, -52, -5, 165, 18)
                 
     
 
-folder_path = r"/Users/munasef/Documents/python/opencv_project/work orders"
-file_path1 = r"/Users/munasef/Documents/python/opencv_project/work orders/58580704  489 JAMESTOWN DR LEHIGHTON  (RMUG)_1.jpg"
-
-ocr1 = ocrByImageRegion(file_path1, folder_path)
-#x = ocr1.get_customer_name()
+#folder_path = r"/Users/munasef/Documents/python/opencv_project/work orders"
+file_path1 = r"/Users/munasef/Documents/python/opencv_project/work orders/58583717  381 SNOWDRIFT RD ANDREAS  (RNSU)_1.jpg"
+ocr1 = ocrByImageRegion(file_path1)
 #print (x)
-#ocr1.get_customer_address()
 #print(x)
-#ocr1.get_job_type()
-#ocr1.get_invoice_number()
-ocr1.get_customer_email()
+print ('\n' + ocr1.get_customer_name() + '\n')
+print ('\n' + ocr1.get_customer_address() + '\n')
+print ('\n' + ocr1.get_job_type() + '\n')
+print ('\n' + ocr1.get_invoice_number() + '\n')
+print ('\n' + ocr1.get_customer_email() + '\n')
 
